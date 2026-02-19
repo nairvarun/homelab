d9ad276857d5ecbf35f4e1dea0718fda334781d8
Varun Nair <nairvarun104@gmail.com>
2026-02-20 01:55:08 +0530

    update bootstrap

--- iac/tf/aws/global/bootstrap/main.tf
+++ iac/tf/aws/global/bootstrap/main.tf
@@ -1,3 +1,33 @@
+terraform {
+  required_version = "~> 1.5"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.33"
+    }
+
+    http = {
+      source  = "hashicorp/http"
+      version = "~> 3.5"
+    }
+  }
+
+  # # initial bootstrap
+  # backend "local" {
+  #   path = "terraform.tfstate"
+  # }
+
+  # move the initial tfstate from local to s3 
+  backend "s3" {
+    bucket         = "homelab-tf-state-85c020c7"
+    key            = "global/bootstrap/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "homelab-tf_lock"
+    encrypt        = true
+  }
+}
+
 provider "aws" {
   region  = var.region
   profile = var.profile
